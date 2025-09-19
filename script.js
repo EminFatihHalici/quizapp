fd60c09ea5bae6728b49dcf0c4cd7504865d85b6
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-09-19 11:49:02 +0200

    feat: showing the questions

--- script.js
+++ script.js
@@ -41,6 +41,19 @@ let questions = [
   },
 ];
 
+let currentQuestion = 0;
+
 function init() {
-    document.getElementById('allQuestions').innerHTML = questions.length;
+  showQuestion();
+  document.getElementById("allQuestions").innerHTML = questions.length;
+}
+
+function showQuestion() {
+  let question = questions[currentQuestion];
+
+  document.getElementById("questiontext").innerHTML = question["question"];
+  document.getElementById("answer_1").innerHTML = question["answer_1"];
+  document.getElementById("answer_2").innerHTML = question["answer_2"];
+  document.getElementById("answer_3").innerHTML = question["answer_3"];
+  document.getElementById("answer_4").innerHTML = question["answer_4"];
 }
